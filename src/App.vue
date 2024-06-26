8f4b308789fbb34539a582aee854a3361cf420a3
YAANNN. <Rakhyan40@gmail.com>
2024-06-26 09:56:47 +0700

    Merge branch 'main' of https://github.com/625536r/Rakhyan-PBK-223510199-Tugas2.git

--- src/App.vue
+++ src/App.vue
@@ -81,7 +81,11 @@ export default {
 
 .text-red {
 <<<<<<< HEAD
+<<<<<<< HEAD
   color: blue
+=======
+  color: yellow
+>>>>>>> 818a2e682e534eee423cb7f4f0b098622c94f11b
 =======
   color: yellow
 >>>>>>> 818a2e682e534eee423cb7f4f0b098622c94f11b
